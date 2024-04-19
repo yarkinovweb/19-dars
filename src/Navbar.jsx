18c7eafdf8dbdbc8fd5109d6ec02cbf8a02c9652
yarkinovweb <yarkinovweb@gmail.com>
2024-04-19 21:31:18 +0500

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1>CodingNepal</h1>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
